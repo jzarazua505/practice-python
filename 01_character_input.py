acbd1112e0ca38845d3de310a3428f0c07099d43
Julian Zarazua <julianzarazua505@gmail.com>
2021-04-20 19:47:13 -0500

    created 01_character_input.py

---
+++ 01_character_input.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+def hundred_yrs():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    hundo_yr = (100 - age) + date.today().year
+    print(f"Hello, {name}, you will be a hundred years old in the year {hundo_yr}.")
+
+def hundred_yrs2():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    hundo_yr = (100 - age) + date.today().year
+    print_count = int(input("How many times do you want this printed? "))
+    print(print_count * f"Hello, {name}, you will be a hundred years old in the year {hundo_yr}.\n", end="")
+
+def hundred_yrs_loop():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    hundo_yr = (100 - age) + date.today().year
+    print_count = int(input("How many times do you want this printed? "))
+    for _ in range(print_count):
+        print(f"Hello, {name}, you will be a hundred years old in the year {hundo_yr}.")
+
+if __name__ == "__main__":
+    hundred_yrs2()
+    hundred_yrs_loop()
