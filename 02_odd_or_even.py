c00d4687fd82994c8f2f3929c98e3936fcf682e4
Julian Zarazua <julianzarazua505@gmail.com>
2021-04-22 18:23:21 -0500

    created 02_odd_or_even.py

---
+++ 02_odd_or_even.py
@@ -0,0 +1,19 @@
+def odd_or_even():
+    num = int(input(" Pick a number: "))
+    if num % 4 == 0:
+        print("The number you picked is a multiple of 4.")
+    elif num % 2 == 0:
+        print("The number you picked is even.")
+    else:
+        print("The number you picked is odd.")
+
+def multiple():
+    num = int(input("Pick a number: "))
+    num2 = int(input("Pick another number: "))
+    if num % num2 == 0:
+        print(f"{num} is a multiple of {num2}.")
+    else:
+        print(f"{num} is not a multiple of {num2}.")
+
+if __name__ == "__main__":
+    multiple()
