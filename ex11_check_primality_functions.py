9d6deac8ca5603cbc116bfda337a9d2fcd4ac687
Julian Zarazua <julianzarazua505@gmail.com>
2021-05-06 19:30:09 -0500

    modified ex11 . created ex12

--- ex11_check_primality_functions.py
+++ ex11_check_primality_functions.py
@@ -1,6 +1,20 @@
 from ex4_divisors import divisors
 
+def primality():
+    num = int(input("What number do you want? "))
+    for n in range(2, num//2):
+        # exits loop as soon as a divisor is found
+        if num % n == 0:
+            return False
+    # if loop completes the number is prime
+    return True            
+            
 if len(divisors()) == 2:
+    print("The number you chose is a prime number.")
+else:
+    print("The number you chose is NOT a prime number.")
+
+if primality():
     print("The number you chose is a prime number.")
 else:
     print("The number you chose is NOT a prime number.")
